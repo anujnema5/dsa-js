97898a69270162dfa234cc60197406f91468602e
Anuj nema <anujnema63@gmail.com>
2023-08-06 01:33:19 +0530

    merge sort array

--- Merge-Sorted-Array.js
+++ Merge-Sorted-Array.js
@@ -1,30 +1,67 @@
-const nums1 = [1,2,3,0,0,0]
-const nums2 = [2,5,6, 9, 10]
+// const nums1 = [1,2,3,0,0,0]
+// const nums2 = [2,5,6, 9, 10]
 
-const merge = (nums1, m, nums2, n) => {
-    debugger
-    let i = m - 1;
-    let j = n - 1;
-    let k = m + n - 1;
+// const merge = (nums1, m, nums2, n) => {
+//     let i = m - 1;
+//     let j = n - 1;
+//     let k = m + n - 1;
 
 
-    while (i >= 0 && j >= 0) {
-      if (nums1[i] > nums2[j]) {
-        nums1[k] = nums1[i];
-        i--;
-      } else {
-        nums1[k] = nums2[j];
-        j--;
-      }
-      k--;
+//     while (i >= 0 && j >= 0) {
+//       if (nums1[i] > nums2[j]) {
+//         nums1[k] = nums1[i];
+//         i--;
+//       } else {
+//         nums1[k] = nums2[j];
+//         j--;
+//       }
+//       k--;
+//     }
+
+//     while (j >= 0) {
+//       nums1[k] = nums2[j];
+//       j--;
+//       k--;
+//     }
+//     console.log(nums1);
+//   };
+
+//   merge(nums1, 3, nums2, 3);
+
+function mergeSortArray(arr1, arr2) {
+
+  if (arr1.length === 0) {
+    return arr2;
+  } else if (arr2.length === 0) {
+    return arr1;
+  }
+
+  const newArray = [];
+  let index = 0;
+  let indexTwo = 0;
+
+  while (index < arr1.length && indexTwo < arr2.length) {
+    if (arr1[index] > arr2[indexTwo]) {
+      newArray.push(arr2[indexTwo])
+      indexTwo++
+    } else {
+      newArray.push(arr1[index])
+      index++
     }
-  
-    while (j >= 0) {
-      nums1[k] = nums2[j];
-      j--;
-      k--;
-    }
-    console.log(nums1);
-  };
-  
-  merge(nums1, 3, nums2, 3);
+  }
+
+  while (index < arr1.length) {
+    newArray.push(arr1[index])
+    index++;
+  }
+
+  while (indexTwo < arr2.length) {
+    newArray.push(arr2[indexTwo])
+    indexTwo++;
+  }
+
+  console.log(newArray);
+  return newArray;
+}
+
+console.log(mergeSortArray([0, 3, 4, 31], [4, 6, 30]));
